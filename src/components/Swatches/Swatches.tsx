d28d6e26fde5a5dcbe03655d403fe01001f6d47d
danielpickett <daniel@danielpickett.me>
2020-04-24 07:13:08 -0400

    save

--- src/components/Swatches/Swatches.tsx
+++ src/components/Swatches/Swatches.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import './Swatches.scss'
 
 export const Swatches = () => {
+  const [isGreyScale, setIsGreyScale] = useState(false)
+  const handleSetIsGreyScale = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsGreyScale(event.currentTarget.checked)
+  }
+
   const colors = ['label', 'primary', 'grey', 'success', 'warning', 'error']
 
   const scale = [
@@ -17,12 +22,7 @@ export const Swatches = () => {
     '900',
   ]
   return (
-    <div className="Swatches">
-      {/* {scale.map((lightness, i) => (
-        <div className="Swatches__color" key={i}>
-          <div className="Swatches__column-label">{lightness}</div>
-        </div>
-      ))} */}
+    <div className="Swatches" style={{filter: isGreyScale ? 'grayscale(100)' : undefined}}>
       {colors.map((color, i) => (
         <div className="Swatches__color" key={i}>
           <div className="Swatches__row-label">
@@ -30,7 +30,7 @@ export const Swatches = () => {
           </div>
           {scale.map((lightness, j) =>
             color === 'label' ? (
-              <div className="Swatches__column-label">{lightness}</div>
+              <div className="Swatches__column-label" key={j}>{lightness}</div>
             ) : (
               <div
                 className={`Swatches__swatch color-${color}-${lightness}`}
@@ -40,6 +40,12 @@ export const Swatches = () => {
           )}
         </div>
       ))}
+      <div className="Swatches__checkbox">
+        <label>
+          <input type="checkbox" checked={isGreyScale} onChange={handleSetIsGreyScale}/> 
+          greyscale
+        </label>
+      </div>
     </div>
   )
 }
